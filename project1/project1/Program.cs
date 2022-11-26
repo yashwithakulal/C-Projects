a0049064f8bd1371dfcac14e03ed3a7dc5db29df
yashwithakulal <113102399+yashwithakulal@users.noreply.github.com>
2022-11-26 08:35:24 +0500

    Add files via upload

---
+++ project1/project1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the radius");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            const double pi = 3.14567;
+            double Area = pi * radius* radius;
+            Console.WriteLine("Radius is:{0},area is:{1}", radius, Area);
+            Console.ReadLine();
+
+            
+         }
+    }
+}
---
+++ project1/project1/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    public class Rectangle
+    {
+        double length;
+        double width;
+        public void Details()
+        {
+            length = 4.5;
+            width = 5.5;
+
+        }
+        public double Area()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("length of rectangle:{0}", length);
+            Console.WriteLine("width of rectangle:{0}", width);
+            Console.WriteLine("Area of Rectangle:{0}", Area());
+
+        }
+    }
+}
